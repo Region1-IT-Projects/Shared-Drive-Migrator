045ed3037a6f6455a8aed3fc63fbdbee4c613424
Jacob Ellington <cannotilever@protonmail.com>
2024-05-20 07:24:28 -0400

    fix scope handling

--- migrator.py
+++ migrator.py
@@ -1,5 +1,14 @@
 import googleapiclient.discovery as gdiscover
+import logging
 import googleapiclient.schema
 from google.oauth2 import service_account
+logging.basicConfig(filename='/tmp/migrator.log', encoding='utf-8', level=logging.DEBUG)
+ACCOUNT_FILE_SRC = 'hvrhs-worker.json'
+SCOPE_LIST = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/admin.directory.user.readonly"]
+credentials = service_account.Credentials.from_service_account_file(ACCOUNT_FILE_SRC, scopes=SCOPE_LIST)
+u = 'jellington@hvrhs.org'
+delegated_credentials = credentials.with_subject(u)
+drive = gdiscover.build('drive', 'v3', credentials=delegated_credentials)
 
-credentials = service_account.Credentials.from_service_account_file()
+driveList: list = drive.drives().list().execute()['drives']
+logging.info("{} has {} team drives.".format(u, len(driveList)))
