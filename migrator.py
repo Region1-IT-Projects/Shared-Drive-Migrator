e139782f28a75171261b20ec31ed1c959c2b7f7b
Jacob Ellington <cannotilever@protonmail.com>
2024-05-25 19:30:13 -0400

    refactor: and I OOP

--- migrator.py
+++ migrator.py
@@ -5,23 +5,37 @@ from google.oauth2 import service_account
 import time
 
 
-def get_all_drive_files(driveID: str, token: str | None = None) -> list[dict]:
-    query_ret: dict = drive.files().list(driveId=driveID, supportsAllDrives=True, corpora="drive", includeItemsFromAllDrives=True, pageToken=token, fields="nextPageToken, files(id, name, mimeType)").execute()
-    file_list: list = query_ret['files']
-    if 'nextPageToken' in query_ret.keys():
-        file_list += get_all_drive_files(driveID, query_ret['nextPageToken'])
-    return file_list
+class Org:
+    def __init__(self, addr, creds: service_account.Credentials):
+        self.delegated_creds = creds.with_subject(addr)
+        self.API = gdiscover.build('drive', 'v3', credentials=self.delegated_creds)
 
 
-ACCOUNT_FILE_SRC = 'hvrhs-worker.json'
-SCOPE_LIST = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/admin.directory.user.readonly"]
-credentials = service_account.Credentials.from_service_account_file(ACCOUNT_FILE_SRC, scopes=SCOPE_LIST)
-u = 'jellington@hvrhs.org'
-delegated_credentials = credentials.with_subject(u)
-drive = gdiscover.build('drive', 'v3', credentials=delegated_credentials)
-driveList: list = drive.drives().list().execute()['drives']
-print("{} has {} team drives.".format(u, len(driveList)))
-print("Collecting files, this might take a while...")
-t_ref = time.time()
-fileList: list = get_all_drive_files(driveList[0]['id'])
-print("Collected {} file handles in {}s.".format(len(fileList), round(time.time() - t_ref, 2)))
+class User:
+    def __init__(self, addr: str, src_creds: service_account.Credentials, dst_creds: service_account.Credentials, src_domain: str, dst_domain: str):
+        self.address = addr
+        self.src = Org(self.address + src_domain, src_creds)
+        self.dst = Org(self.address + dst_domain, dst_creds)
+        self.team_drives: list = self.src.API.drives().list().execute()['drives']
+
+    def get_all_drive_files(self, driveID: str, token: str | None = None) -> list[dict]:
+        query_ret: dict = self.src.API.files().list(driveId=driveID, supportsAllDrives=True, corpora="drive",
+                                                includeItemsFromAllDrives=True, pageToken=token,
+                                                fields="nextPageToken, files(id, name, mimeType)").execute()
+        file_list: list = query_ret['files']
+        if 'nextPageToken' in query_ret.keys():
+            file_list += self.get_all_drive_files(driveID, query_ret['nextPageToken'])
+        return file_list
+
+
+def main(src_token: str, dst_token: str, src_domain: str, dst_domain: str):
+    SCOPE_LIST = ["https://www.googleapis.com/auth/drive",
+                  "https://www.googleapis.com/auth/admin.directory.user.readonly"]
+    src_creds: service_account.Credentials = service_account.Credentials.from_service_account_file(src_token,
+                                                                                                   scopes=SCOPE_LIST)
+    dst_creds: service_account.Credentials = service_account.Credentials.from_service_account_file(dst_token,
+                                                                                                   scopes=SCOPE_LIST)
+    test_u = User("jellington", src_creds, dst_creds, src_domain, dst_domain)
+
+
+main("hvrhs-worker.json", "", "hvrhs.org", "region1schools.org")
